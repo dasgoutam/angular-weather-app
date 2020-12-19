f1163cd63ab697104e5932dd4de7a4c7b4beb926
Goutam Das <Goutam_Das@Dell.com>
2020-12-20 02:25:25 +0530

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { ParticlesModule } from "angular-particle";
 import { AppComponent } from "./app.component";
 import { OpenweatherService } from "./openweather.service";
 import { WeatherCardComponent } from "./weather-card/weather-card.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   declarations: [AppComponent, WeatherCardComponent],
   bootstrap: [AppComponent],
   providers: [HttpClientModule, OpenweatherService]
